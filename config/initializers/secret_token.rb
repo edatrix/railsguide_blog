3d3a99a389bfb507a1e54a475e92e59f00982508
edatrix <lespath@gmail.com>
2014-05-12 12:03:42 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsguideBlog::Application.config.secret_key_base = 'ad226754b7e28d9b565443f1e2553ff047acb61ae26b4eacb528b50a8f6ab813f8300e5d82b14b9d088752304f5e974099369bb41612ef3be78bf1444e2ab0f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsguideBlog::Application.config.session_store :cookie_store, key: '_railsguide_blog_session'
