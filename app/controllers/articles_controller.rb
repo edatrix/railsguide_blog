3523c173f502c4501f633b63a30e28a82e13ca53
edatrix <lespath@gmail.com>
2014-05-13 17:47:47 -0600

    through 9.2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "jude",
+                               password: "baby",
+                               except: [:index, :show]
+
   def new
     @article = Article.new
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,9 @@
 class CommentsController < ApplicationController
 
+  http_basic_authenticate_with name: "jude",
+                               password: "baby",
+                               only: :destroy
+
   def create
     @article = Article.find(params[:article_id])
     @comments = @article.comments.create(comment_params)
